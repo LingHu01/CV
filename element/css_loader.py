8b47d26629e46c0b8be26ab63a0f45deda5a135d
LingHu01 <michel11.zhao@gmail.com>
2023-09-28 18:59:57 +0200

    loading in base of device type: phone done

--- element/css_loader.py
+++ element/css_loader.py
@@ -5,19 +5,20 @@ def apply_landscape_styles():
     # Replace this with your landscape styles
     document.body.classList.remove('phone')
     document.body.classList.add('desktop')
+    show_content()
 
 # Function to apply portrait styles
 def apply_portrait_styles():
     # Replace this with your portrait styles
     document.body.classList.remove('desktop')
     document.body.classList.add('phone')
+    show_content()
 
 # Function to show the content
 def show_content():
     # Select and show the content elements
-    content_elements = document.querySelectorAll('.main-grid, .header, .sidebar, .main')
-    for element in content_elements:
-        element.style.display = 'block'
+    element = document.querySelector('.main-grid')
+    element.style.display = 'grid'
 
 # Check screen dimensions
 def check_screen_dimensions():
@@ -28,8 +29,4 @@ def check_screen_dimensions():
         # Portrait orientation
         apply_portrait_styles()
 
-# Wait for the DOM to fully load before running the code
-document.addEventListener('DOMContentLoaded', check_screen_dimensions)
-
-# Once the code has executed, show the content
-window.addEventListener('load', show_content)
+check_screen_dimensions()
