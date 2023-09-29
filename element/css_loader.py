2cc1d9db88b2b7ef2c2ee74b4c88204ebec8cdf8
LingHu01 <michel11.zhao@gmail.com>
2023-09-29 12:59:04 +0200

    fade away loading screen

--- element/css_loader.py
+++ element/css_loader.py
@@ -1,33 +1,25 @@
 from pyscript import document, window
 
-# Function to apply landscape styles
 def apply_landscape_styles():
-    # Replace this with your landscape styles
     document.body.classList.remove('phone')
     document.body.classList.add('desktop')
-    show_content()
 
-# Function to apply portrait styles
 def apply_portrait_styles():
-    # Replace this with your portrait styles
     document.body.classList.remove('desktop')
     document.body.classList.add('phone')
-    show_content()
 
-# Function to show the content
 def show_content():
-    # Select and show the content elements
     element = document.querySelector('.main-grid')
     element.style.display = 'grid'
 
-# Check screen dimensions
 def check_screen_dimensions():
     if window.innerWidth > window.innerHeight:
-        # Landscape orientation
         apply_landscape_styles()
     else:
-        # Portrait orientation
         apply_portrait_styles()
-    document.querySelector('.loading-screen').style.display = 'none'
+    loading_screen = document.querySelector('.loading-screen')
+    loading_screen.classList.add('hidden')
+    loading_screen.addEventListener('transitionend', lambda event: loading_screen.parentNode.removeChild(loading_screen))
+    show_content()
 
 check_screen_dimensions()
